8616a80872e4d8d5b2ada3d7b48c636122090684
Dalton Keel <daltonkeel02@gmail.com>
2024-12-03 18:44:22 -0500

    Solved Day 3 P2.

--- 2024/3/driver.py
+++ 2024/3/driver.py
@@ -10,44 +10,35 @@ def readFile(filePath: str) -> list[str]:
 
     return lines
 
-def findMul(line: str) -> int:
+def findMul(line: str, pattern: str, parse: bool = True) -> tuple[int, bool]:
     total: int = 0
-    pattern: str = r'mul\(\d+,\d+\)'
-    digitPattern: str = r'\d+'
     matches: list[str] = re.findall(pattern, line)
 
     for match in matches:
-        num1, num2 = re.findall(digitPattern, match)
-        num1, num2 = int(num1), int(num2)
-        total += (num1 * num2)
+        if match == 'do()':
+            parse = True
+        elif match == "don't()":
+            parse = False
+        else:
+            if parse:
+                num1, num2 = map(int, re.findall(r'\d+', str(match)))
+                total += num1 * num2
 
-    return total
-
-def findDoMul(line: str) -> int:
-    total: int = 0
-    pattern: str = r'mul\(\d+,\d+\)'
-    aroundDo: str = r"(.*?)(?=don't\(\))|(?<=do\(\))(.*?)(?=don't\(\)|$)"
-    digitPattern: str = r'\d+'
-    chunks: list[str] = re.findall(aroundDo, line)
-
-    for chunk in chunks:
-        mulMatch: list[str] = re.findall(pattern, str(chunk))
-        for match in mulMatch:
-            num1, num2 = re.findall(digitPattern, match)
-            num1, num2 = int(num1), int(num2)
-            total += (num1 * num2)
-
-    return total
+    return total, parse
 
 def main() -> None:
     lines = readFile('input.txt')
     muls: list[int] = []
     dos: list[int] = []
-    for line in lines:
-        muls.append(findMul(line))
-        dos.append(findDoMul(line))
+    mulPattern: str = r'mul\(\d+,\d+\)'
+    doPattern: str = rf"{mulPattern}|do\(\)|don't\(\)"
+    parse: bool = True
 
-    #ans < 147910205
+    for line in lines:
+        total, _ = findMul(line, mulPattern)
+        muls.append(total)
+        total, parse = findMul(line, doPattern, parse)
+        dos.append(total)
 
     print(f'Muls: {sum(muls)}\nDos: {sum(dos)}')
 
