07b17f35ed3fe8f8b0bd628d5557ecc6e2a33d72
Keel, Dalton (hxa7pu) <hxa7pu@uvawise.edu>
2025-12-04 11:07:41 -0500

    Solved D4P2.

--- 2025/4/driver.py
+++ 2025/4/driver.py
@@ -12,13 +12,13 @@ def readFile(filePath: str) -> list[str]:
 def checkValid(lines: list[str], maxNeighbors: int) -> tuple[int, list[tuple[int,int]]]:
     valids: int = 0
     directions: list[tuple[int, int]] = [
-                                         (-1, -1)   ,
-                                         (-1,  0)   ,
-                                         (-1,  1)   ,
-                                         ( 0, -1)   ,
-                                         ( 0,  1)   ,
-                                         ( 1, -1)   ,
-                                         ( 1,  0)   ,
+                                         (-1, -1),
+                                         (-1,  0),
+                                         (-1,  1),
+                                         ( 0, -1),
+                                         ( 0,  1),
+                                         ( 1, -1),
+                                         ( 1,  0),
                                          ( 1,  1)
                                         ]
     locations: list[tuple[int, int]] = []
@@ -26,27 +26,19 @@ def checkValid(lines: list[str], maxNeighbors: int) -> tuple[int, list[tuple[int
         for col in range(len(lines[row])):
             if lines[row][col] == '@':
                 foundNeighbors: int = 0
-                if (0 <= row < len(lines) and 0 <= col < len(lines[row])) and lines[row][col] == '@':
+                if (0 <= row < len(lines) and 0 <= col < len(lines[row]))\
+                and lines[row][col] == '@':
                     for roww, coll in directions:
                         if (row + roww > len(lines)-1)\
                         or (col+coll > len(lines)-1)\
                         or (row+roww < 0)\
                         or (col+coll < 0):
-                            #print("Out of bounds")
                             continue
                         if lines[row + roww][col + coll] == '@':
-                            #print("Currently on row, col:", row, col)
-                            #print("Found neighbor at row, col:", row+roww, col+coll)
-                            #print(lines[row+roww])
                             foundNeighbors += 1
                 if foundNeighbors < maxNeighbors:
                     locations.append((row,col))
                     valids += 1
-                #print("-----STATE------")
-                #print(*lines, sep='\n')
-                #print("Neighbors: ", foundNeighbors, "At row,col:", row, col)
-                #print(locations)
-                #print("-----STATE------")
 
     return valids, locations
 
@@ -59,16 +51,35 @@ def showLocations(lines: list[str], locations: list[tuple[int, int]]):
                 print(lines[row][col], end='')
         print()
 
+def removeRolls(lines: list[str], locations: list[tuple[int, int]]) -> tuple[list[str], int]:
+    removed: int = 0
+    for row in range(len(lines)):
+        for col in range(len(lines[row])):
+            if (row, col) in locations:
+                removed += 1
+                lines[row] = lines[row][:col] + '.' + lines[row][col+1:]
+    return lines, removed
+
 
 def main():
     lines: list[str] = readFile('sample.txt')
+    newLines: list[str] = []
     count: int = 0
     count2: int = 0
 
     count, locations = checkValid(lines, 4)
     showLocations(lines, locations)
-    #count2 += int()
 
+    while locations:
+        newLines, removed = removeRolls(lines, locations)
+        _, locations = checkValid(newLines, 4)
+        lines = newLines
+        count2 += removed
+
+    print()
+    print()
+
+    showLocations(newLines, locations)
 
     print("Part 1:", count)
     print("Part 2:", count2)
