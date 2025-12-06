5dca32bc166590cea87711484bbdebaae41d506b
Dalton <daltonkeel02@gmail.com>
2025-12-06 14:46:04 -0500

    D6P2 refactor

--- 2025/6/driver.py
+++ 2025/6/driver.py
@@ -1,33 +1,48 @@
 #!/bin/python3
 
-def readFile(filePath: str) -> list[list[str]]:
-    lines: list[list[str]] = []
+def readFile(filePath: str) -> list[str]:
+    lines: list[str] = []
 
     with open(filePath, 'r') as f:
-        #lines = f.read().splitlines()
-        for line in f:
-            lines.append(line.split())
+        lines = f.read().splitlines()
 
     return lines
 
+def stripLines(lines: list[str]) -> list[list[str]]:
+    newLines: list[list[str]] = []
+
+    for line in lines:
+        newLines.append(line.split())
+
+    return newLines
+
+def convertMatrix(matrix: list[list[str]]) -> list[list[str]]:
+    newMatrix: list[list[str]] = []
+
+    for col in range(len(matrix[0])):
+        curCol: list[str] = []
+        for row in range(len(matrix)):
+            val: str = ''
+            val = matrix[row][col]
+            curCol.append(val)
+        newMatrix.append(curCol)
+
+    return newMatrix
+    
 def solveLines(lines: list[list[str]]) -> int:
-    numRows: int = len(lines)
-    numCols: int = len(lines[0])
     nums: list[list[int]] = []
     ops: list[str] = []
-    operations: list[str] = ['*', '/', '+', '-']
+    operations: list[str] = ['*', '+']
     solved: list[int] = []
 
-    for col in range(numCols):
-        curCol: list[int] = []
-        for row in range(numRows):
-            val: str = ''
-            val = lines[row][col]
+    for row in lines:
+        curRow: list[int] = []
+        for val in row:
             if val in operations:
                 ops.append(val)
             else:
-                curCol.append(int(val))
-        nums.append(curCol)
+                curRow.append(int(val))
+        nums.append(curRow)
 
     for idx, op in enumerate(ops):
         total: int = 0
@@ -43,12 +58,12 @@ def solveLines(lines: list[list[str]]) -> int:
     return sum(solved)
 
 def main():
-    lines: list[list[str]] = readFile('given.txt')
+    lines: list[str] = readFile('given.txt')
     count: int = 0
     count2: int = 0
+    matrix: list[list[str]] = convertMatrix(stripLines(lines))
 
-    count = solveLines(lines)
-
+    count = solveLines(matrix)
 
     print("Part 1:", count)
     print("Part 2:", count2)
